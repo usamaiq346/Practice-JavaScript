4e93664207e547e89be954bb9ce7cfc9278b9e5a
usamaiq346 <usamaiq346@gmail.com>
2025-10-22 17:17:30 +0000

    Refactor array concatenation examples and add spread operator demonstration

--- javascript/02_basics/02_arrays.js
+++ javascript/02_basics/02_arrays.js
@@ -4,10 +4,14 @@ const dcHeroes = [" SuperMan", "Batman","AquaMan"]
 // console.log(dcHeroes)
 
 // marvelHeroes.push(dcHeroes)
-// console.log (marvelHeroes) //['IronMan','SpiderMan','AntMan', [ ' SuperMan', 'Batman', 'AquaMan' ] ]
+// console.log(marvelHeroes[3][0])  // SuperMan 3 index which is another array then pick o index element
+//console.log (marvelHeroes) //['IronMan','SpiderMan','AntMan', [ ' SuperMan', 'Batman', 'AquaMan' ] ]
 // // push add both arrays but 2nd array add as songle elemnt 
 
-const concatAllHeroes = marvelHeroes.concat(dcHeroes)
-console.log(concatAllHeroes) // [ 'IronMan', 'SpiderMan', 'AntMan', ' SuperMan', 'Batman', 'AquaMan' ]
+// const concatAllHeroes = marvelHeroes.concat(dcHeroes)
+// console.log(concatAllHeroes) // [ 'IronMan', 'SpiderMan', 'AntMan', ' SuperMan', 'Batman', 'AquaMan' ]
 // concat add both add arrays and this time the add all arrays from 2nd arrays as single elemnets
 // one more thing concat need another variable to store values
+
+const spreadAllHeroes = [...marvelHeroes, ...dcHeroes]
+console.log(spreadAllHeroes) // 
