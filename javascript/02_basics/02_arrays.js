9127a2df2d1519aaab48d23c124b331f64e88ab5
usamaiq346 <usamaiq346@gmail.com>
2025-10-23 13:42:20 +0000

    Add examples for Array methods: isArray, from, and of

--- javascript/02_basics/02_arrays.js
+++ javascript/02_basics/02_arrays.js
@@ -14,4 +14,20 @@ const dcHeroes = [" SuperMan", "Batman","AquaMan"]
 // one more thing concat need another variable to store values
 
 const spreadAllHeroes = [...marvelHeroes, ...dcHeroes]
-console.log(spreadAllHeroes) // 
+// console.log(spreadAllHeroes) // 
+
+// const nestedArray = [1,2,3,[5,6,7,],6,4,6,[8,9,0]]
+// const flatNestedArray = nestedArray.flat() // flat(infinity) both working as same
+// console.log(flatNestedArray) // [ 1, 2, 3, 5, 6,7, 6, 4, 6, 8,9, 0] // flat perform nested array into singlr    
+
+// conversion of all data type into array 
+console.log(Array.isArray("Array")) // False bcz its string 
+console.log(Array.from("Array")) // [ 'A', 'r', 'r', 'a', 'y' ] // convert string into arrray 
+// .from working on single varaible
+console.log(Array.from({name: "Array"})) // [] give empty array bcz of it can't distinguish what to be return 
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3)) // [ 100, 200, 300 ] // its can convert multilple variiable return into single array 
+
