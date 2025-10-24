d020967ea3b59e0e40f76081ef03059b395fe8a5
usamaiq346 <usamaiq346@gmail.com>
2025-10-24 15:59:06 +0000

    Enhance userObject function with examples and demonstrate usage of rest parameters in calculateCartPrice function

--- javascript/03_basics/01_functions.js
+++ javascript/03_basics/01_functions.js
@@ -53,6 +53,57 @@ function loginUserMessage (userName = "Login User"){
     }
     return `${userName } just login right Now`
 }
-console.log(loginUserMessage()) // please Enter your Name First
-console.log(loginUserMessage()) // Login User just login right Now // if assign default value in parameter so is will print atleast default value  
+// console.log(loginUserMessage()) // please Enter your Name First
+// console.log(loginUserMessage()) // Login User just login right Now // if assign default value in parameter so is will print atleast default value  
 // console.log(loginUserMessage("Usama")) // Usama just login right Now
+
+// function calculateCartPrice (num1){
+//     return num1
+// }
+// console.log(calculateCartPrice(2)) // 2
+
+// function calculateCartPrice (num1){
+//     return num1
+// }
+// console.log (calculateCartPrice(10, 20, 30,40)) // 10 bcz it only take first value so to overcome this issue we use another method
+
+// function calculateCartPrice (...num1){
+//     return num1
+// }
+// console.log(calculateCartPrice(10, 20, 30, 40)) // [ 10, 20, 30, 40 ] by using rest operator as like spread operator it work same and convert into single array 
+
+// function calculateCartPrice (val1, val2, ...num1){
+//     return num1
+// }
+// console.log (calculateCartPrice(10, 20, 30, 40)) // [ 30, 40 ] in this val1 hold first value and val2 hold secomd value and remaining value store in  num1
+
+const user = {
+    name : "usama",
+    price : 10
+}
+function userObject (anyObject){
+    // console.log (`User name is ${anyObject.name} and there price was ${anyObject.price}`)
+}
+// userObject(user) // User name is usama and there price was 10
+// console.log(userObject()) // error was missing arguments
+// console.log(userObject(user)) // User name is usama and there price was 10
+
+// we can directly insert in function arugument that can change  the the user value
+userObject  ({
+    name : "Iqbal",
+    price : 20
+}) // User name is Iqbal and there price was 20
+// console.log(userObject(user))// givinn error bcz function already define
+
+//  For Array function
+// function myArray (anyArray){
+//     return anyArray 
+// }
+// console.log(myArray([10, 20, 30, 40])) // [ 10, 20, 30, 40 ]
+
+const getSecondArray = [100, 200, 300, 400,]
+function myarray (getArray){
+    return getArray[2]
+}
+console.log(myarray(getSecondArray)) // 300
+
